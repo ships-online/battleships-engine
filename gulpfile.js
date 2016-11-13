3eb0eb26e77bdd48beedf1d0fa336b0a9f253a58
Oskar Wrobel <kontakt@oskarwrobel.pl>
2016-11-13 17:10:05 +0100

    Added relink task.

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,21 @@ const config = {
 const path = require( 'path' );
 const gulp = require( 'gulp' );
 const del = require( 'del' );
-const utils = require( 'battleships-dev-tools/lib/utils.js' )( config );
+
+const utils = require( 'battleships-dev-tools/lib/utils.js' );
+const linkTask = require( 'battleships-dev-tools/lib/tasks/relink.js' )( config );
 const lintTasks = require( 'battleships-dev-tools/lib/tasks/lint.js' )( config );
 const testTasks = require( 'battleships-dev-tools/lib/tasks/test.js' )( config );
 const compileTasks = require( 'battleships-dev-tools/lib/tasks/compile.js' )( config );
 
 const options = utils.parseArgs( process.argv.slice( 3 ) );
 
+gulp.task( 'relink', linkTask.relink );
+
 // Compile engine to esnext format.
 gulp.task( 'clean:compile:utils', () => del( './lib/utils' ) );
-gulp.task( 'compile:utils', [ 'clean:compile:utils' ], () => compileTasks.compile( '../battleships-utils/src', './lib/utils' ) );
+gulp.task( 'compile:utils', [ 'clean:compile:utils' ], () =>
+	compileTasks.compile( 'node_modules/battleships-utils/src', './lib/utils' ) );
 gulp.task( 'compile', [ 'compile:utils' ], ( done ) => done() );
 
 // JS code sniffer.
