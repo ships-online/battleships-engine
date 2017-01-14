dcf324357be0fd0aac462af05b9985f70af9a7ef
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-01-14 20:06:05 +0100

    Removed compilation dev task.

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,13 @@ const path = require( 'path' );
 const gulp = require( 'gulp' );
 
 const utils = require( 'battleships-dev-tools/lib/utils.js' );
-const linkTask = require( 'battleships-dev-tools/lib/tasks/relink.js' )( config );
+const relinkTask = require( 'battleships-dev-tools/lib/tasks/relink.js' )( config );
 const lintTasks = require( 'battleships-dev-tools/lib/tasks/lint.js' )( config );
 const testTasks = require( 'battleships-dev-tools/lib/tasks/test.js' )( config );
-const compileTasks = require( 'battleships-dev-tools/lib/tasks/compile.js' )( config );
 
 const options = utils.parseArgs( process.argv.slice( 3 ) );
 
-gulp.task( 'relink', linkTask.relink );
-
-// Build dependency.
-gulp.task( 'build:utils', () => compileTasks.buildDependency( 'utils' ) );
-gulp.task( 'build', [ 'build:utils' ], ( done ) => done() );
+gulp.task( 'relink', relinkTask.relink );
 
 // JS code sniffer.
 const jsFiles = [ path.join( config.ROOT_PATH, '**', '*.js' ) ];
