56ea0781ee22615f93104b62e327ca75994cf75e
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-06-25 14:47:46 +0200

    Removed link task.

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,10 @@ const config = {
 const gulp = require( 'gulp' );
 
 const utils = require( 'battleships-dev-tools/lib/utils.js' );
-const relinkTask = require( 'battleships-dev-tools/lib/tasks/relink.js' )( config );
 const lintTasks = require( 'battleships-dev-tools/lib/tasks/lint.js' )( config );
 const testTasks = require( 'battleships-dev-tools/lib/tasks/test.js' )( config );
 
 const options = utils.parseArgs( process.argv.slice( 3 ) );
-
-gulp.task( 'relink', relinkTask.relink );
 
 gulp.task( 'lint', lintTasks.lint );
 gulp.task( 'pre-commit', lintTasks.lintStaged );
